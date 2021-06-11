232fa7d44005aaeb93506b742a2e5212483d772e
Ryan Hewitt <ryan@mesuva.com.au>
2021-06-11 14:55:53 +0100

    - Add in column and classes to better indicate success status of uploads

--- blocks/community_store_file_upload/view.php
+++ blocks/community_store_file_upload/view.php
@@ -4,127 +4,133 @@
 <?php if (\Concrete\Core\Page\Page::getCurrentPage()->isEditMode()) { ?>
     <div class="ccm-edit-mode-disabled-item"><?= t('File Uploads'); ?></div>
 <?php } else { ?>
-<form class="store-fileupload" method="post" enctype="multipart/form-data" action="<?= $offerUploads ? $view->action('upload') : $view->action('find'); ?>">
-    <?= $token->output('community_store'); ?>
+    <form class="store-fileupload" method="post" enctype="multipart/form-data" action="<?= $offerUploads ? $view->action('upload') : $view->action('find'); ?>">
+        <?= $token->output('community_store'); ?>
 
-    <?php
+        <?php
 
-    $fieldcount = 0;
+        $fieldcount = 0;
 
-    if ($offerUploads) { ?>
+        if ($offerUploads) { ?>
 
-        <h2><?= t('Upload Files for Order #%s', $order->getOrderID()); ?></h2>
+            <h2><?= t('Upload Files for Order #%s', $order->getOrderID()); ?></h2>
 
-        <?php if (isset($actions)) { ?>
-            <?= $successText; ?>
-        <?php } else { ?>
-            <?= $promptText; ?>
-        <?php } ?>
+            <?php if (isset($actions)) { ?>
+                <?= $successText; ?>
+            <?php } else { ?>
+                <?= $promptText; ?>
+            <?php } ?>
 
-        <table class="table table-striped" id="store-fileupload-table">
-            <thead>
-            <tr>
-                <th><strong><?= t("Item"); ?></strong></th>
-                <th class=""><?= t("File Uploads"); ?></th>
-            </tr>
-            </thead>
-            <tbody>
-
-            <?php foreach ($fields as $field) { ?>
+            <table class="table table-striped" id="store-fileupload-table">
+                <thead>
                 <tr>
+                    <th><strong><?= t("Item"); ?></strong></th>
+                    <th class=""><?= t("Status"); ?></th>
+                    <th class=""><?= t("File Uploads"); ?></th>
+                </tr>
+                </thead>
+                <tbody>
+
+                <?php foreach ($fields as $field) { ?>
                     <?php
                     $file = $field['file'];
                     $item = $field['item'];
                     ?>
 
-                    <?php if ($field['count'] == 1  ) { ?>
-                    <td rowspan="<?= ($field['quantity'] ?  $field['quantity'] : 1); ?>"><p><?= h($item->getProductName()) ?>
-                        <?php if ($sku = $item->getSKU()) {
-                            echo '(' . h($sku) . ')';
-                        } ?>
-                        </p>
+                    <tr <?= $file ? 'class="upload-completed"' : ''; ?>>
 
-                        <?php
-                        $options = $item->getProductOptions();
-                        if ($options) {
-                            echo "<ul class='list-unstyled'>";
-                            foreach ($options as $option) { ?>
-                                <li><strong><?= h($option['oioKey']); ?></strong> <?= h($option['oioValue']) ? h($option['oioValue']) : '<em>' . t('None') . '</em>'; ?></li>
-                            <?php }
-                            echo "</ul>";
-                        }
-                        ?>
+                        <td <?= $file ? 'class="bg-success"' : ''; ?>><p><?= h($item->getProductName()) ?>
+                                <?php if ($sku = $item->getSKU()) {
+                                    echo '(' . h($sku) . ')';
+                                } ?>
+                            </p>
 
-                    </td>
-                    <?php } ?>
+                            <?php
+                            $options = $item->getProductOptions();
+                            if ($options) {
+                                echo "<ul class='list-unstyled'>";
+                                foreach ($options as $option) { ?>
+                                    <li><strong><?= h($option['oioKey']); ?></strong> <?= h($option['oioValue']) ? h($option['oioValue']) : '<em>' . t('None') . '</em>'; ?></li>
+                                <?php }
+                                echo "</ul>";
+                            }
+                            ?>
 
-                    <td>
-                        <div class="form-group">
-                            <?php if ($file || ($file && $allowReplacing)) { ?>
-                            <label class="control-label" for="<?= h($field['field']); ?>">
-                                <?php if ($file && $allowReplacing) { ?>
-                                    <?= t('Replace'); ?>
-                                <?php } ?>
-
-                                <?php if ($file) { ?>
-                                    <em><?= h($file->getTitle()); ?></em>
-                                <?php } ?>
-                            </label>
-                            <?php } ?>
-                            <?php if (!$file || $allowReplacing) { ?>
-                                <input type="file" class="form-control" name="<?= h($field['field']); ?>" id="<?= h($field['field']); ?>">
-
-                                <?php $fieldcount++;
-                            } ?>
-
-                            <?php if ($field['label'] || $field['quantity'] > 1) { ?>
-                            <span class="help-block"><?= h($field['label']); ?>
-                            <?php if ($field['quantity'] > 1) { ?>
-                                <?= ($field['label'] ? ' - ' : '') . t('File') . ' ' . $field['count']; ?>
-                                <?php }?>
-                            </span>
+                        </td>
+                        <td class="store-fileupload-result">
+                            <?php if ($file) { ?>
+                                <?= t('File Successfully Uploaded'); ?>
+                            <?php } else { ?>
+                                <?= t('To be uploaded'); ?>
                             <?php } ?>
 
-                        </div>
-                    </td>
+                        </td>
+                        <td>
+                            <div class="form-group">
+                                <?php if ($file || ($file && $allowReplacing)) { ?>
+                                    <label class="control-label" for="<?= h($field['field']); ?>">
+                                        <?php if ($file && $allowReplacing) { ?>
+                                            <?= t('Replace'); ?>
+                                        <?php } ?>
 
-                </tr>
+                                        <?php if ($file) { ?>
+                                            <em><?= h($file->getTitle()); ?></em>
+                                        <?php } ?>
+                                    </label>
+                                <?php } ?>
+                                <?php if (!$file || $allowReplacing) { ?>
+                                    <input type="file" class="form-control" name="<?= h($field['field']); ?>" id="<?= h($field['field']); ?>">
 
+                                    <?php $fieldcount++;
+                                } ?>
+
+                                <?php if ($field['label']) { ?>
+                                    <span class="help-block"><?= h($field['label']); ?></span>
+                                <?php } ?>
+
+                            </div>
+                        </td>
+
+
+                    </tr>
+
+                <?php } ?>
+            </table>
+
+            <?php if ($fieldcount > 0) { ?>
+                <p class="text-right">
+                    <button type="submit" class="btn btn-primary"><?= $buttonLabel ? h($buttonLabel) : t('Upload') ?></button>
+                </p>
+            <?php } else { ?>
+                <p class="alert alert-info"><?= $completedText ? h($completedText) : t('All files have been uploaded'); ?></p>
+                <style>
+                    #store-fileupload-table {
+                        display: none;
+                    }
+                </style>
             <?php } ?>
-        </table>
 
-        <?php if ($fieldcount > 0) { ?>
-            <p class="text-right"><button type="submit" class="btn btn-primary"><?= $buttonLabel ? h($buttonLabel) : t('Upload') ?></button></p>
-        <?php } else { ?>
-            <p class="alert alert-info"><?= $completedText ? h($completedText) : t('All files have been uploaded'); ?></p>
-            <style>
-                #store-fileupload-table {
-                    display: none;
-                }
-            </style>
+        <?php } elseif ($allowSearching) { ?>
+
+            <h2><?= t('Find Order'); ?></h2>
+
+            <?php if (isset($notFound)) { ?>
+                <p class="alert alert-danger"><?= t('No order was found matching that order number and email'); ?></p>
+            <?php } ?>
+
+            <div class="form-group">
+                <label class="control-label" for="email"><?= t('Order Number'); ?></label>
+                <input type="number" name="order_number" <?php if (isset($submittedOrderNumber)) { ?> value="<?= h($submittedOrderNumber); ?>"<?php } ?> required class="form-control"/>
+            </div>
+
+            <div class="form-group">
+                <label class="control-label" for="email"><?= t('Email'); ?></label>
+                <input type="email" name="email" <?php if (isset($submittedEmail)) { ?> value="<?= h($submittedEmail); ?>"<?php } ?> required class="form-control"/>
+            </div>
+
+            <button type="submit" class="btn btn-primary"><?= t('Find Order') ?></button>
+
         <?php } ?>
 
-    <?php } elseif ($allowSearching) { ?>
-
-        <h2><?= t('Find Order'); ?></h2>
-
-        <?php if (isset($notFound)) { ?>
-            <p class="alert alert-danger"><?= t('No order was found matching that order number and email'); ?></p>
-        <?php } ?>
-
-        <div class="form-group">
-            <label class="control-label" for="email"><?= t('Order Number'); ?></label>
-            <input type="number" name="order_number" <?php if (isset($submittedOrderNumber)) { ?> value="<?= h($submittedOrderNumber); ?>"<?php } ?> required class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label class="control-label" for="email"><?= t('Email'); ?></label>
-            <input type="email" name="email" <?php if (isset($submittedEmail)) { ?> value="<?= h($submittedEmail); ?>"<?php } ?> required class="form-control"/>
-        </div>
-
-        <button type="submit" class="btn btn-primary"><?= t('Find Order') ?></button>
-
-    <?php } ?>
-
-</form>
+    </form>
 <?php } ?>
